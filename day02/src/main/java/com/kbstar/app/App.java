f786586bb44709dbbbb4efc86d6b125d4658001a
lydia-hr2 <124110284+lydia-hr2@users.noreply.github.com>
2023-05-08 19:03:02 +0900

    Delete day02 directory

--- day02/src/main/java/com/kbstar/app/App.java
+++
@@ -1,9 +0,0 @@
-package com.kbstar.app;
-import com.kbstar.frame.TV;
-import com.kbstar.tv.*;
-public class App {
-    public static void main(String args[]){
-        TV tv = new STV();
-        tv.turnOn();
-    }
-}
