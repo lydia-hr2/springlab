a279a99a10d2ad7f312d2d642fe6b87d59930790
lydia-hr2 <124110284+lydia-hr2@users.noreply.github.com>
2023-05-23 17:51:46 +0900

    Delete web22 directory

--- web22/src/main/java/com/kbstar/dto/Question.java
+++
@@ -1,9 +0,0 @@
-package com.kbstar.dto;
-
-import lombok.Data;
-
-@Data
-public class Question {
-    private String name;
-    private String msg;
-}
--- web22/src/main/java/com/kbstar/kakaopay/AmountVO.java
+++
@@ -1,9 +0,0 @@
-package com.kbstar.kakaopay;
-
-import lombok.Data;
-
-@Data
-public class AmountVO {
-
-    private Integer total, tax_free, vat, point, discount;
-}
